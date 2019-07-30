ac88763642348b68005615c467306b0c019757ea
jimCresswell <jim.cresswell@gmail.com>
2019-07-30 19:55:39 +0100

    Styling

--- src/index.js
+++ src/index.js
@@ -122,7 +122,7 @@ class Game extends React.Component {
           />
         </div>
         <div className="game-info">
-          <div>{status}</div>
+          <div className={winner?'winner':''}>{status}</div>
           <ol>{moves}</ol>
         </div>
       </div>
