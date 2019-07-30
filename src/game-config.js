e4c4955ea5571342620ad6ed58ba4ad0ebf8f145
jimCresswell <jim.cresswell@gmail.com>
2019-07-30 19:14:50 +0100

    More traditional player characters

--- src/game-config.js
+++ src/game-config.js
@@ -1,6 +1,6 @@
 const boardWidth = 3;
 const boardHeight = 3;
 const playerOneChar = 'X';
-const playerTwoChar = 'Y';
+const playerTwoChar = 'O';
 
 export {boardWidth, boardHeight, playerOneChar, playerTwoChar};
