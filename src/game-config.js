71036741aa27d6c3074a0de8c06419101bf4bdc0
jimCresswell <jim.cresswell@gmail.com>
2019-07-31 13:56:33 +0100

    Thematically relevant player characters

--- src/game-config.js
+++ src/game-config.js
@@ -1,7 +1,7 @@
 // Board width, height, winning length.
 const characteristicLength = 3;
 
-const playerOneChar = 'X';
-const playerTwoChar = 'O';
+const playerOneChar = '🚀';
+const playerTwoChar = '🛰️';
 
 export {characteristicLength, playerOneChar, playerTwoChar};
