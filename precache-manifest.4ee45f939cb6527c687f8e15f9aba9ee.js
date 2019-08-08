02668f5b5dee38bdd0109fd1991ae8de5df11165
jimCresswell <jim.cresswell@gmail.com>
2019-08-08 08:29:52 +0100

    Updates

---
+++ precache-manifest.4ee45f939cb6527c687f8e15f9aba9ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39901701568b513c929036d98ecaca05",
+    "url": "/triple-t/index.html"
+  },
+  {
+    "revision": "57bcb0aa5e7632613a84",
+    "url": "/triple-t/static/css/main.77964c88.chunk.css"
+  },
+  {
+    "revision": "b6a704ca644c1df5127f",
+    "url": "/triple-t/static/js/2.77bfa541.chunk.js"
+  },
+  {
+    "revision": "57bcb0aa5e7632613a84",
+    "url": "/triple-t/static/js/main.bac0e181.chunk.js"
+  },
+  {
+    "revision": "9efb508c8f7f620aa78c",
+    "url": "/triple-t/static/js/runtime~main.6f8007f3.js"
+  },
+  {
+    "revision": "906002dcdf4b0f48e9fac365b37f0808",
+    "url": "/triple-t/static/media/another-world-intro.906002dc.mp3"
+  },
+  {
+    "revision": "3463876051c3107c541f5ea05639837b",
+    "url": "/triple-t/static/media/r-type-title.34638760.mp3"
+  },
+  {
+    "revision": "957ac6e33e94eb76080a74f9446fb7b3",
+    "url": "/triple-t/static/media/shadow-of-the-beast-intro.957ac6e3.mp3"
+  },
+  {
+    "revision": "294b4888708bc73064371480d23c18de",
+    "url": "/triple-t/static/media/wopr_super.294b4888.jpg"
+  }
+]);
